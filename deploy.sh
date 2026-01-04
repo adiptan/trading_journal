a4fb62351a43fd5cfb325bc27beaaf5a01261418
aleksandrdiptan <diptan@huntflow.ru>
2026-01-04 11:37:26 +0300

    Remove files not needed

--- deploy.sh
+++
@@ -1,4 +0,0 @@
-docker stop $(docker ps -a | grep forum | awk '{print $1}' | awk '(NR==1)')
-docker rm $(docker ps -a | grep forum | awk '{print $1}' | awk '(NR==1)')
-docker image rm $(docker images | grep forum | awk '{print $3}' | awk '(NR==1)')
-docker-compose up
--- test.py
+++
@@ -1,9 +0,0 @@
-from environs import Env
-
-env = Env()
-env.read_env()
-
-MAIN_PHOTO = env("MAIN_PHOTO")
-ADMIN_GROUP_ID = env("ADMIN_GROUP_ID")
-
-print(int(-2777288165))
